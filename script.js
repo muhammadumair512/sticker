662eedc4618257daf03ad7d61400626d7db17c9f
unknown <umair@gmil.com>
2024-11-11 13:11:32 +0500

    updted

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Function to update gradient based on device tilt with increased sensitivity
 function updateGradient(tiltX, tiltY) {
   const gradientCircle = document.querySelector(".gradient-circle");
 
-  // Increase sensitivity by scaling tilt values
+  // Adjust sensitivity by scaling tilt values
   const angleX = (Math.min(Math.max(tiltX, -90), 90) + 90) * 1.5;
   const angleY = (Math.min(Math.max(tiltY, -90), 90) + 90) * 1.5;
   const angle = (angleX * 0.7 + angleY * 0.3) % 360;
 
-  // Update the gradient angle dynamically
   gradientCircle.style.background = `
       radial-gradient(circle at 50% 50%, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.3) 25%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0) 75%) 50% 50%,
       radial-gradient(circle at 50% 50%, rgba(240, 230, 140, 0.3) 0%, rgba(250, 250, 210, 0.2) 30%, rgba(240, 230, 140, 0) 70%) 50% 50%,
