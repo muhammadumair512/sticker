2c158bdad96cf394dc38072ba68b7c142f73b5fe
unknown <umair@gmil.com>
2024-11-16 17:24:12 +0500

    overall adj

--- svg gradient - option 3/script.js
+++ svg gradient - option 3/script.js
@@ -236,6 +236,6 @@ const text = document.querySelector(".text p");
 text.innerHTML = text.innerText
   .split("")
   .map(
-    (char, i) => `<span style="transform:rotate(${i * 14}deg)">${char}</span>`
+    (char, i) => `<span style="transform:rotate(${i * 7}deg)">${char}</span>`
   )
   .join("");
